2644dbd07b2a99d0b6c2310e1da99a522fa88532
pie-L0pes <lopes.f.v.pietra@gmail.com>
2026-05-06 08:26:54 -0300

    criação do backend

---
+++ api/api/server.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const estadiaRoutes = require('./src/routes/estadia.routes');
+
+app.use('/estadia', estadiaRoutes);
+
+
+const automovelRoutes = require('./src/routes/automovel.routes');
+
+app.use('/automovel', automovelRoutes);
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
